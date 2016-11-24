579b2229e974557904171c9f7734798e594d54b1
Pascalusa <usa.pascal@yahoo.fr>
2016-11-24 12:25:28 +0100

    algo

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+class statistique extends PHPUnit_Framework_TestCase
+{
+
+    public function testMultiplication(){
+
+    // Avec le vrai Fuseau horaire
+
+    while ($nombre_de_conexion <= 100)
+    $date = date("d-m-Y");
+    $heure = date("H:i");
+    {
+    echo 'Ceci est la conexion n°' . $nombre_de_conexion . ' . $date . ' '<br />';
+    $nombre_de_lignes++;
+    }
+     
+    {
+
+}
